4818c5c558442c89c8d3436bb85b9bb967365516
solyess <czjone@126.com>
2022-06-20 14:03:50 +0800

    Update xlib.lua

--- xlib.lua
+++ xlib.lua
@@ -1,6 +1,6 @@
 xlib = xlib or {}
 
----------------------------------------------------
+-- ==========================================================================
 -- xlib.class
 function class(super)
     if (type(super) == "string") then
@@ -49,7 +49,7 @@ function class(super)
     return class_type
 end
 
----------------------------------------------------
+-- ==========================================================================
 -- table.extention
 function table.indexof(_table, val)
     for pos, v in ipairs(_table) do
@@ -60,7 +60,7 @@ function table.indexof(_table, val)
     return -1, nil;
 end
 
----------------------------------------------------
+-- ==========================================================================
 -- xlib.core
 xlib.core = xlib.core or {}
 xlib.core.base = class()
@@ -74,7 +74,7 @@ function base:dector()
         self[k] = nil;
     end
 end
----------------------------------------------------
+------------------------------------------------
 -- xlib.core.eventdispather
 xlib.core.eventdispather = class(xlib.core.base)
 local eventdispather = xlib.core.eventdispather
@@ -129,8 +129,87 @@ function eventdispather:dispatch(key, ...)
         end
     end
 end
+
+------------------------------------------------
+-- loger
+xlib.logger = class()
+function xlib.logger:ctor()
+    self._print = print;
+    print = nil;
+end
+
+function xlib.logger:info(...)
+    self._print("[INFO]", ...)
+end
+
+function xlib.logger:error(...)
+    self._print("[ERROR]", ...)
+    error(...);
+end
+
+function xlib.logger:fail(...)
+    self._print("[FAIL]", ...)
+end
+
+function xlib.logger:assert(exp, des)
+    if (exp == false) then
+        self:error(des)
+    end
+end
+
+--------------------------------------------------
+-- memory
+xlib.memory = xlib.memory or class()
+
+function xlib.memory:monitor(func, name)
+    local start = collectgarbage("count");
+    collectgarbage("collect")
+    collectgarbage("collect")
+    collectgarbage("collect")
+    collectgarbage("collect")
+    local ret = func();
+    collectgarbage("collect")
+    collectgarbage("collect")
+    collectgarbage("collect")
+    collectgarbage("collect")
+    xlib.log:info(name .. "(memory leak):", collectgarbage("count") - start);
+    return ret;
+end
+
 ---------------------------------------------------
+-- test
+xlib.test = xlib.test or class()
+function xlib.test:ctor()
+    self._test = {}
+end
+function xlib.test:put(func, des)
+    table.insert(self._test, {
+        f = func,
+        msg = des
+    });
+    return self;
+end
+
+function xlib.test:run()
+    local memory = xlib.memory.new();
+    local count, success, fail = #self._test, 0, 0
+    for _, vt in ipairs(self._test) do
+        if (not memory:monitor(vt.f, vt.msg)) then
+            xlib.log:fail(vt.msg);
+            fail = fail + 1
+        else
+            success = success + 1
+        end
+    end
+    xlib.log:info(string.format("execute:%u success:%u fail:%u", count, success, fail));
+    memory = nill
+end
+
+-- ==========================================================================
+-- xlib.ecs
 xlib.ecs = xlib.ecs or {}
+
+----------------------------------------------------------
 -- xlib.ecs.base
 xlib.ecs.base = class(xlib.core.eventdispather)
 local ecsbase = xlib.ecs.base;
@@ -142,23 +221,26 @@ function ecsbase:getcontext()
     return self._context
 end
 
+----------------------------------------------------------
 -- xlib.ecs.entity
 xlib.ecs.entity = class(xlib.ecs.base)
 local entity = xlib.ecs.entity
-entity.event = {
-    on_add_component = 1,
-    on_remove_component = 2,
-    on_replace_component = 3,
-    on_release_entity = 4,
-    on_destroy_entity = 5
-}
+entity.event = {}
+entity.event.on_add_component = 1
+entity.event.on_remove_component = 2
+entity.event.on_replace_component = 3
+entity.event.on_release_entity = 4
+entity.event.on_destroy_entity = 5
+
 function entity:ctor()
     self._coms = {}
 end
+
 function entity:dector()
     self:dispatch(self.event.on_destroy_entity);
     entity.super.dector(self)
 end
+
 function entity:add(com)
     table.insert(self._coms, com);
     self:dispatch(self.event.on_add_component, com);
@@ -174,6 +256,7 @@ function entity:replace(com)
     return self;
 end
 
+------------------------------------------------------------
 -- xlib.ecs.component
 xlib.ecs.component = class(xlib.ecs.base)
 local component = xlib.ecs.component
@@ -181,6 +264,7 @@ function component:ctor()
     self._typeid = -1;
 end
 
+------------------------------------------------------------
 -- xlib.ecs.componentPool
 xlib.ecs.component_pool = class(xlib.ecs.base)
 local component_pool = xlib.ecs.component_pool
@@ -208,12 +292,13 @@ function component_pool:get(component)
     return nil;
 end
 
+------------------------------------------------------------
 -- xlib.ecs.system
 xlib.ecs.system = class(xlib.ecs.base)
 local system = xlib.ecs.system;
 
 function system:ctor(name)
-    self._name = name
+    self.name = name
     self._inited = false
     self._child_system = {}
 end
@@ -229,7 +314,7 @@ function system:remove(sys)
     local is_string = type == "string"
     for i = #self._child_system, 1, -1 do
         local _sys = self._child_system[i];
-        if (is_table and _sys == sys) or (is_string and _sys._name == sys._name) then
+        if (is_table and _sys == sys) or (is_string and _sys.name == sys.name) then
             table.remove(self._child_system, i);
         end
     end
@@ -259,87 +344,46 @@ function system:cleanup()
     self._child_system = nil
 end
 
+xlib.ecs.matcher = class({})
+
+-- function system:filter()
+--     return {};
+-- end
+
+------------------------------------------------------------
 -- xlib.ecs.feature
 xlib.ecs.feature = class(xlib.ecs.system)
 local feature = xlib.ecs.feature
 
--- loger
-xlib.logger = class()
-function xlib.logger:ctor()
-    self._print = print;
-    print = nil;
-end
+-- -- ==========================================================================
+-- -- xlib.unity
+-- xlib.unity = xlib.unity or {}
+-- xlib.unity.assets = class(xlib.core.eventdispather)
+-- xlib.unity.application = class({})
+-- xlib.unity.util = class({})
 
-function xlib.logger:info(...)
-    self._print("[INFO]", ...)
-end
+-- -- ==========================================================================
+-- -- xlib.mvcvm
+-- xlib.mvcvm = xlib.mvcvm or {}
+-- -- xlib.mvcvm
+-- xlib.mvcvm.base = class(xlib.core.eventdispather)
+-- xlib.mvcvm.context = class(xlib.core.eventdispather)
+-- xlib.mvcvm.application = class(xlib.core.eventdispather)
+-- xlib.mvcvm.model = class(xlib.mvcvm.base)
+-- xlib.mvcvm.view = class(xlib.mvcvm.base)
+-- xlib.mvcvm.controller = class(xlib.mvcvm.base)
+-- xlib.mvcvm.view_model = class(xlib.mvcvm.base)
 
-function xlib.logger:error(...)
-    self._print("[ERROR]", ...)
-    error(...);
-end
-
-function xlib.logger:fail(...)
-    self._print("[FAIL]", ...)
-end
-
-function xlib.logger:assert(exp, des)
-    if (exp == false) then
-        self:error(des)
-    end
-end
-
-xlib.log = xlib.logger.new();
+--------------------------------------------------
 -- check env.
-xlib.log:assert(_VERSION == "Lua 5.2" or _VERSION == "Lua 5.3" or _VERSION == "Lua 5.4", "lua env must 5.2 or later")
--- out env
-xlib.log:info(tostring(_VERSION))
---------------------------------------------------
--- memory
-xlib.memory = xlib.memory or class()
+xlib.log = xlib.logger.new();
+xlib.log:assert(_VERSION == "Lua 5.2" or _VERSION == "Lua 5.3" or _VERSION == "Lua 5.4", "lua env must 5.2 or later") -- check env.
+xlib.log:info(tostring(_VERSION)) -- out env
 
-function xlib.memory:monitor(func, name)
-    local start = collectgarbage("count");
-    collectgarbage("collect")
-    local ret = func();
-    collectgarbage("collect")
-    xlib.log:info(name .. "(memory leak):", collectgarbage("count") - start);
-    return ret;
-end
-
----------------------------------------------------
--- test
-xlib.test = xlib.test or class()
-function xlib.test:ctor()
-    self._test = {}
-end
-function xlib.test:put(exp, des)
-    table.insert(self._test, {
-        f = exp,
-        msg = des
-    });
-    return self;
-end
-function xlib.test:run()
-    local memory = xlib.memory.new();
-    local count, success, fail = #self._test, 0, 0
-    for _, vt in ipairs(self._test) do
-        if (not memory:monitor(vt.f, vt.msg)) then
-            xlib.log:fail(vt.msg);
-            fail = fail + 1
-        else
-            success = success + 1
-        end
-    end
-    xlib.log:info(string.format("execute:%u success:%u fail:%u", count, success, fail));
-    memory = nill
-end
-
---------------------------------------------------
+-- ==========================================================================
 -- test class.
 
 -- test components
-
 local com_player = class(xlib.ecs.component)
 com_player.coin = 0;
 com_player.gem = 0;
@@ -363,47 +407,125 @@ com_remote_host.port = nil
 -- test entity
 local game_entity = class(xlib.ecs.entity)
 function game_entity:ctor()
-    self:add(com_player):add(com_player_info):add(com_player_view):add(com_remote_host)
+    self:add(com_player.new())
+    self:add(com_player_info.new())
+    self:add(com_player_view.new())
+    self:add(com_remote_host.new())
 end
 -- end test entity
 
 -- logic sysetm
 local ui_init_system = class(xlib.ecs.system);
+function ui_init_system:ctor()
+    self._name = "ui_init_system"
+end
+
 local player_system = class(xlib.ecs.system);
+function player_system:ctor()
+    self.name = "player_system"
+end
+
 local ui_render_system = class(xlib.ecs.system);
+function ui_render_system:ctor()
+    self.name = "ui_render_system"
+end
+
 local ui_input_system = class(xlib.ecs.system);
+function ui_input_system:ctor()
+    self.name = "ui_input_system"
+end
+
 local ui_destory_system = class(xlib.ecs.system);
+function ui_destory_system:ctor()
+    self.name = "ui_input_system"
+end
 
 local network_connect_sytem = class(xlib.ecs.system);
+function network_connect_sytem:ctor()
+    self.name = "network_connect_sytem"
+end
+
 local network_write_system = class(xlib.ecs.system);
+function network_write_system:ctor()
+    self.name = "network_write_system"
+end
+
 local network_read_system = class(xlib.ecs.system);
+function network_read_system:ctor()
+    self.name = "network_read_system"
+end
 
 local ui_system = class(xlib.ecs.feature);
 function ui_system:ctor()
-    self:add(ui_init_system.new()):add(ui_render_system.new()):add(ui_input_system.new()):add(ui_destory_system.new())
+    self.name = "ui_system"
+    self:add(ui_init_system.new())
+    self:add(ui_render_system.new())
+    self:add(ui_input_system.new())
+    self:add(ui_destory_system.new())
 end
 
 local network_system = class(xlib.ecs.feature);
 function network_system:ctor()
-    self:add(network_connect_sytem.new()):add(network_write_system.new()):add(network_read_system.new())
+    self.name = "network_system";
+    self:add(network_connect_sytem.new())
+    self:add(network_write_system.new())
+    self:add(network_read_system.new())
 end
+
+local feature_test = class(xlib.ecs.feature)
+function feature_test:ctor()
+    self.name = "feature_test"
+    self:add(ui_system.new())
+    self:add(network_system.new())
+end
+
+-- function feature_test:dector()
+--     feature_test.super.dector(self);
+-- end
 
 --------------------------------------------------
 -- test case.
-xlib.test.new():put(function()
-    return true;
-end, "class"):put(function()
-    return true;
-end, "loger"):put(function()
-    local loop = 3 -- min = 1
-    local feature = xlib.ecs.feature.new()
-    feature:add(ui_system.new()):add(network_system.new())
-    feature:initialize();
-    for i = 1, loop, 1 do
-        feature:execute()
+local test = xlib.test.new()
+test:put(function()
+    for i = 1, 100000, 1 do
+        local com = xlib.ecs.component.new();
+        com.id = 13;
+        com.ref = {};
     end
+    return true;
+end, "class memory")
+
+test:put(function()
+    local feature = feature_test.new()
+    feature:initialize();
+    feature:execute();
     feature:cleanup();
     return true;
-end, "ecs"):run()
+end, "ECS A")
+
+test:put(function()
+    local feature = feature_test.new()
+    feature:initialize();
+    feature:execute();
+    feature:cleanup();
+    return true;
+end, "ECS B")
+
+test:put(function()
+    local feature = feature_test.new()
+    feature:initialize();
+    feature:execute();
+    feature:cleanup();
+    return true;
+end, "ECS C")
+
+test:put(function()
+    local feature = feature_test.new()
+    feature:initialize();
+    feature:execute();
+    feature:cleanup();
+    return true;
+end, "ECS D")
+test:run()
 
 return xlib.ecs
